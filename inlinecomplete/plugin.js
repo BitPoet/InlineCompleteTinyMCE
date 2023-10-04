6da4325f632494c9dc8dc0881e17909f2191e32c
Chris <BitPoet@users.noreply.github.com>
2023-10-04 19:51:23 +0200

    Language support + v0.0.5
    Add experimental language support.
    First work on supporting multi-trigger actions.
    Bump version to 0.0.5.

--- inlinecomplete/plugin.js
+++ inlinecomplete/plugin.js
@@ -2,16 +2,45 @@ jQuery(document).ready(function() {
 	
 	InputfieldTinyMCE.onReady(function(editor) {
 		
+		var langid = false;
 		var fieldname = editor.id.replace('Inputfield_', '');
-		var config = ProcessWire.config['InputfieldTinyMCE_' + fieldname].inlinecomplete;
+		if(/__\d+$/.test(fieldname)) {
+			langid = fieldname.replace(/^.*__/, '');
+			var baseFieldname = fieldname.replace(/__\d+$/, '');
+			var config = ProcessWire.config['InputfieldTinyMCE_' + baseFieldname];
+		} else {
+			var config = ProcessWire.config['InputfieldTinyMCE_' + fieldname];
+		}
 		
-		for(var i = 0, l = config.actions.length; i < l; i++) {
-			
-			var autocompleteConfig = InlineCompleteHandler(editor, config, config.actions[i]);
-    		editor.ui.registry.addAutocompleter(fieldname + '_autocomplete' + i, autocompleteConfig);
-    		
-    	}
-    	
+		if(config && config.inlinecomplete) {
+
+			var triggerNum = 0;			
+			for(var i = 0, l = config.inlinecomplete.actions.length; i < l; i++) {
+				
+				if(Array.isArray(config.inlinecomplete.actions[i].trigger)) {
+					var triggers = config.inlinecomplete.actions[i].trigger;
+					for(var k = 0; k < triggers.length; k++) {
+						var actionSettings = Object.assign({}, config.inlinecomplete.actions[i]);
+						actionSettings.trigger = triggers[k];
+						if(langid)
+							actionSettings.language = langid;
+						var autocompleteConfig = InlineCompleteHandler(editor, config.inlinecomplete, actionSettings);
+			    		editor.ui.registry.addAutocompleter(fieldname + '_autocomplete' + triggerNum, autocompleteConfig);
+			    		triggerNum++;
+					}
+				} else {
+					var actionSettings = Object.assign({}, config.inlinecomplete.actions[i]);
+					if(langid)
+						actionSettings.language = langid;
+					var autocompleteConfig = InlineCompleteHandler(editor, config.inlinecomplete, actionSettings);
+		    		editor.ui.registry.addAutocompleter(fieldname + '_autocomplete' + triggerNum, autocompleteConfig);
+		    		triggerNum++;
+				}
+				
+	    		
+	    	}
+		}
+		    	
 	});
 
 });
@@ -33,7 +62,9 @@ function InlineCompleteHandler(editor, config, act) {
 					page:			act.page,
 					field:			act.name,
 					action:			act.action,
-					filter:			pattern
+					filter:			pattern,
+					trigger:		act.trigger,
+					language:		act.language
 				},
 				method:		'POST'
 			}).then((data) => {resolve(data)});
